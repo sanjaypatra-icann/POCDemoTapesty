095121fa6796982ca1978e6b72c8f61f7ce9ddb3
Sanjay Patra <sanjay.patra@icann.org>
2017-04-20 20:04:50 +0530

    class has added text

--- src/main/TestDemo.java
+++ src/main/TestDemo.java
@@ -4,10 +4,15 @@ package main;
  * Created by SP48716 on 19-04-2017.
  */
 public class TestDemo {
-
-
+String str[]=null;
+    public void setValues(String[] somevalues) {
+        this.str = somevalues;
+    }
     public static void main(String[] arg){
         System.out.println("Hello");
+
+
+
 
 
     }
